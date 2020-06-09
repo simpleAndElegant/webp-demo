e1397fba2dc8ca32c82a30b65ab7f2a29a9dda9c
liangrui <964385822@qq.com>
2020-06-09 23:29:30 +0800

    demo

---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  plugins: [
+    require('webp-in-css/plugin'),
+    require('autoprefixer')
+  ]
+};
--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <picture>
+      <source class="img" srcset="./assets/logo.webp" type="image/webp">
+      <img src="./assets/logo.png" alt="Screenshot">
+    </picture>
+
+    <div class="demo"> 
+
+    </div>
   </div>
 </template>
 
@@ -24,5 +31,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.demo {
+  width: 30px;
+  height: 30px;
+  background: url('./assets/logo.png');
+}
+.img {
+  width: 200px;
+  height: 200px;
 }
 </style>
